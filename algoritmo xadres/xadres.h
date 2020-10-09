d1ec3abe65050c9f3d030e9edd26119604eef807
Vitor Caires <39932465+Vitor-Caires@users.noreply.github.com>
2020-10-09 20:30:53 -0300

    Update xadres.h

--- algoritmo xadres/xadres.h
+++ algoritmo xadres/xadres.h
@@ -6,6 +6,8 @@ typedef struct {
     char tipo;
 }peca;
 
+int tab_danger[8][8];//tabuleiro de ataques p/ xeques
+
 void moverPeca(peca tab[][8], int x0, int y0, int x1, int y1);
 int movTorre(peca tab[][8], int new_x, int new_y, int turno, char p);
 int movBispo(peca tab[][8], int new_x, int new_y, int turno, char p);
@@ -14,3 +16,4 @@ int movRainha(peca tab[][8], int new_x, int new_y, int turno);
 int movRei(peca tab[][8], int new_x, int new_y, int turno);
 int movPeao(peca tab[][8], int new_x, int new_y, int turno,int cap);
 void showGame(peca tab[][8]);
+int update_danger(peca tab[][8],int *tab_danger_temp[][8]);
