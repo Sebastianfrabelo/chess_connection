eb48929fedd37c143b74097ff1ec5d74304aed34
VitorLeonardi <61316876+VitorLeonardi@users.noreply.github.com>
2020-10-09 23:28:11 -0300

    Update xadres.c

--- algoritmo xadres/xadres.c
+++ algoritmo xadres/xadres.c
@@ -11,112 +11,108 @@ void moverPeca(peca tab[][8], int x0, int y0, int x1, int y1) {
     tab[x0][y0].jog = -1;
 }
 
-int movTorre(peca tab[][8], int new_x, int new_y, int turno, char p) {
-    for (int i = new_x + 1; i < 8; i++) {
-        if (tab[i][new_y].jog == turno && tab[i][new_y].tipo == p) {
-            moverPeca(tab, i, new_y, new_x, new_y);
-            return 1;
+int movTorre(peca tab[][8], int new_x, int new_y, int turno) {
+    // procura por uma torre na horizontal e vertical do local escolhido 
+    int sq[] = {1, 0, -1, 0};
+    int i, j, x, y, t;
+    for (int k = 0; k < 4; k++) {
+        t = 1;
+        x = i = sq[k];
+        y = j = sq[(k + 1) % 4];
+        while (x < 8 && x > -1 && y < 8 && y > -1) {
+            if (tab[x][y].jog == turno && tab[x][y].tipo == 'r') {
+                moverPeca(tab, x, y, new_x, new_y);
+                return 1;
+            }
+            else if (tab[x][y].jog != -1) break;
+            t++;
+            x = new_x + (t * i);
+            y = new_y + (t * j);
         }
-        else if (tab[i][new_y].jog != -1) break;
-
-    }
-
-    for (int i = new_x - 1; i >= 0; i--) {
-        if (tab[i][new_y].jog == turno && tab[i][new_y].tipo == p) {
-            moverPeca(tab, i, new_y, new_x, new_y);
-            return 1;
-        }
-        else if (tab[i][new_y].jog != -1) break;
-    }
-
-    for (int j = new_y + 1; j < 8; j++) {
-        if (tab[new_x][j].jog == turno && tab[new_x][j].tipo == p) {
-            moverPeca(tab, new_x, j, new_x, new_y);
-            return 1;
-        }
-        else if (tab[new_x][j].jog != -1) break;
-    }
-
-    for (int j = new_y - 1; j >= 0; j--) {
-        if (tab[new_x][j].jog == turno && tab[new_x][j].tipo == p) {
-            moverPeca(tab, new_x, j, new_x, new_y);
-            return 1;
-        }
-        else if (tab[new_x][j].jog != -1) break;
     }
     return 0;
 }
 
-int movBispo(peca tab[][8], int new_x, int new_y, int turno, char p) {
-    for (int i = 1; new_x + i < 8 && new_y + i < 8; i++) {
-        if (tab[new_x + i][new_y + i].jog == turno && tab[new_x + i][new_y + i].tipo == p) {
-            moverPeca(tab, new_x + i, new_y + i, new_x, new_y);
-            return 1;
-        }
-        else if (tab[new_x + i][new_y + i].jog != -1) break;
-    }
 
-    for (int i = 1; new_x - i >= 0 && new_y - i >= 0; i++) {
-        if (tab[new_x - i][new_y - i].jog == turno && tab[new_x - i][new_y - i].tipo == p) {
-            moverPeca(tab, new_x - i, new_y - i, new_x, new_y);
-            return 1;
-        }
-        else if (tab[new_x - i][new_y - i].jog != -1) break;
-    }
-
-    for (int i = 1; new_x + i < 8 && new_y - i >= 0; i++) {
-        if (tab[new_x + i][new_y - i].jog == turno && tab[new_x + i][new_y - i].tipo == p) {
-            moverPeca(tab, new_x + i, new_y - i, new_x, new_y);
-            return 1;
-        }
-        else if (tab[new_x + i][new_y - i].jog != -1) break;
-    }
-
-    for (int i = 1; new_y + i < 8 && new_x - i >= 0; i++) {
-        if (tab[new_x - i][new_y + i].jog == turno && tab[new_x - i][new_y + i].tipo == p) {
-            moverPeca(tab, new_x - i, new_y + i, new_x, new_y);
-            return 1;
-        }
-        else if (tab[new_x - i][new_y + i].jog != -1) break;
-    }
-    return 0;
-}
 
 int movCavalo(peca tab[][8], int new_x, int new_y, int turno) {
+    //procura por um cavalo
     int cav[4] = { -2,-1,1,2 };
-    int t;
+    int x, y;
     for (int i = 0; i < 4; i++) {
-        t = 3 - abs(cav[i]);
-        if (new_x + cav[i] > 7 || new_x + cav[i] < 0) continue;
-
-        if (new_y + t < 7 && new_y + t >= 0) {
-            if (tab[new_x + cav[i]][new_y + t].jog == turno && tab[new_x + cav[i]][new_y + t].tipo == 'n') {
-                moverPeca(tab, new_x + cav[i], new_y + t, new_x, new_y);
+        x = new_x + cav[i];
+        y = new_y + 3 - abs(cav[i]);
+        if (x > 7 || x < 0) continue;
+        if (y < 7 && y >= 0) {
+            if (tab[x][y].jog == turno && tab[x][y].tipo == 'n') {
+                moverPeca(tab, x, y, new_x, new_y);
                 return 1;
             }
         }
-        if (new_y - t < 7 && new_y - t >= 0) {
-            if (tab[new_x + cav[i]][new_y - t].jog == turno && tab[new_x + cav[i]][new_y - t].tipo == 'n') {
-                moverPeca(tab, new_x + cav[i], new_y - t, new_x, new_y);
+        y = new_y - 3 + abs(cav[i]);
+        if (y < 7 && y >= 0) {
+            if (tab[x][y].jog == turno && tab[x][y].tipo == 'n') {
+                moverPeca(tab, x, y, new_x, new_y);
                 return 1;
             }
         }
     }
     return 0;
+}
+
+int movBispo(peca tab[][8], int new_x, int new_y, int turno) {
+    // procura por um bispo nas diagonais do local escolhido 
+    int sq[] = { 1, 1, -1, -1 };
+    int i, j, x, y, t;
+    for (int k = 0; k < 4; k++) {
+        t = 1;
+        x = i = sq[k];
+        y = j = sq[(k + 1) % 4];
+        while (x < 8 && x > -1 && y < 8 && y > -1) {
+            if (tab[x][y].jog == turno && tab[x][y].tipo == 'b') {
+                moverPeca(tab, x, y, new_x, new_y);
+                return 1;
+            }
+            else if (tab[x][y].jog != -1) break;
+            t++;
+            x = new_x + (t * i);
+            y = new_y + (t * j);
+        }
+    }
+    return 0; 
 }
 
 int movRainha(peca tab[][8], int new_x, int new_y, int turno) {
-    int f;
-    f = movTorre(tab, new_x, new_y, turno, 'q');
-    if (f == 0) movBispo(tab, new_x, new_y, turno, 'q');
-    return f;
+    // procura pela rainha nas diagonais, horizontal e vertical do local escolhido 
+    int sq[] = {0,1,1,1,0,-1,-1,-1};
+    int i, j, x, y, t;
+    for (int k = 0; k < 8; k++) {
+        t = 1;
+        x = i = sq[k];
+        y = j = sq[(k + 2) % 8];
+        while (x < 8 && x > -1 && y < 8 && y > -1) {
+            if (tab[x][y].jog == turno && tab[x][y].tipo == 'q') {
+                moverPeca(tab, x, y, new_x, new_y);
+                return 1;
+            }
+            else if (tab[x][y].jog != -1) break;
+            t++;
+            x = new_x + (t * i);
+            y = new_y + (t * j);
+        }
+    }
+    return 0;
 }
 
 int movRei(peca tab[][8], int new_x, int new_y, int turno) {
+    int x, y;
     for (int i = -1; i < 2; i++) {
         for (int j = -1; j < 2; j++) {
-            if ((tab[new_x + i][new_y + j].jog == turno && tab[new_x + i][new_y + j].tipo == 'k') && (tab_danger[new_x][new_y] == turno) || (tab_danger[new_x][new_y] == -1) ) {
-                moverPeca(tab, new_x + i, new_y + j, new_x, new_y);
+            x = new_x + i;
+            y = new_y + j;
+            if (x == -1 || x == 8 || y == -1 || y == 8) break;
+            if (tab[x][y].jog == turno && tab[x][y].tipo == 'k') {
+                moverPeca(tab, x, y, new_x, new_y);
                 return 1;
                 break;
             }
@@ -126,23 +122,28 @@ int movRei(peca tab[][8], int new_x, int new_y, int turno) {
 
 }
 
+
 int movPeao(peca tab[][8], int new_x, int new_y, int turno, int cap) {
-    int t = 2 * turno - 1;
+    int y = new_y + 2 * turno - 1;
+    int x;
     if (cap == 0) {
-        if (tab[new_x][new_y + t].jog == turno && tab[new_x][new_y + t].tipo == 'p') {
-            moverPeca(tab, new_x, new_y + t, new_x, new_y);
+        x = new_x;
+        if (tab[x][y].jog == turno && tab[x][y].tipo == 'p') {
+            moverPeca(tab, x, y, new_x, new_y);
             return 1;
         }
     }else{
         if (new_x < 7) {
-            if (tab[new_x + 1][new_y + t].jog == turno && tab[new_x + 1][new_y + t].tipo == 'p') {
-                moverPeca(tab, new_x - 1 , new_y + t, new_x, new_y);
+            x = new_x + 1;
+            if (tab[x][y].jog == turno && tab[x][y].tipo == 'p') {
+                moverPeca(tab, x , y, new_x, new_y);
                 return 1;
             }
         }
         if (new_x > 0) {
-            if (tab[new_x - 1][new_y + t].jog == turno && tab[new_x - 1][new_y + t].tipo == 'p') {
-                moverPeca(tab, new_x - 1, new_y + t, new_x, new_y);
+            x = new_x - 1;
+            if (tab[x][y].jog == turno && tab[x][y].tipo == 'p') {
+                moverPeca(tab, x, y, new_x, new_y);
                 return 1;
             }
         }
